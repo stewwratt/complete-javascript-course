c041d107fc0e125076ff51827fe0273fc2948238
Joshua Stewart <js@jsMBPro.local>
2021-12-27 08:07:49 +1100

    MONDAY 27/12 - back at it - Completed this section - Garnered what i hope to be quite a deep understanding of closures and how they are encountered

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,155 @@
 'use strict';
+
+// BEGINNING AT CODING CHALLENGE 1 (LOST ALL PROGRESS) 23/12
+// Will code along to ease myself back into the coding mindset
+// A little disheartened with the loss of all the work i had
+// done. Clean Slate, Lets go.
+
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // this generates [0, 0, 0, 0]. More in the next section
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      answer >= 0 &&
+      this.answers[answer]++;
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+// poll.registerNewAnswer();
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 4, 2, 9] });
+*/
+
+// Immediately Invoked Function Expressions (IIFE)
+
+/*
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+//IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('this will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 23;
+}
+// console.log(isPrivate)
+console.log(notPrivate);
+*/
+
+// Closures
+
+/*
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+// Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 23);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// RE-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+// Coding Challenge #2
+
+/*
+// More of a thinking challenge
+
+// Got this right and understood that the closure appeared here
+// Used an arrow function whereas Jonas used a function declaration
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+//   document.querySelector('body').addEventListener('click', () => {
+//     header.style.color = 'blue';
+//   });
+// })();
+
+// Jonas' solution
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+*/
